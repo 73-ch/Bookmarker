08657b2782144923fbccd102279699a495b5b1bd
nami634 <cigarbox.nami@gmail.com>
2016-09-26 01:56:28 +0900

    bookmarkのレベルをstorageに保存・取り出す機能を追加。また、storage全体のデータを取ってくるメソッドも実装。

--- background.js
+++ background.js
@@ -1,13 +1,28 @@
 var desktop_bookmarks;
 chrome.runtime.onMessage.addListener(
   function(message,sender,sendResponse) {
-    console.log(message);
-    if (message.greeting == "hello") {
+    if (message.get_bookmark) {
       getBookmark().then(function(bookmarks){
         sendResponse({bookmarks: bookmarks});
       });
-      return true;
     };
+    if (message.bookmark_level){
+      setBookmarkLevel(message.bookmark_level.bookmark_id, message.bookmark_level.level).then(function(result){
+        sendResponse({result: result});
+      });
+    };
+    if (message.getBookmarkLevel){
+      getBookmarkLevel(message.getBookmarkLevel.bookmark_id).then(function(result){
+        sendResponse({result: result});
+      });
+    };
+    if (message.getStorage){
+      getStorage().then(function(result){
+        console.log(result);
+        sendResponse({result: result});
+      });
+    };
+    return true;
   }
 );
 
@@ -16,6 +31,31 @@ function getBookmark(){
     chrome.bookmarks.getSubTree("1", function(callback){
       var desktop_bookmarks = callback[0].children; // bookmarkの階層が人によって違うかも？
       resolve(desktop_bookmarks);
+    });
+  });
+}
+
+function setBookmarkLevel(bookmark_id, level){
+  return new Promise(function(resolve, reject){
+    var data = {};
+    data[bookmark_id] = level;
+    chrome.storage.local.set(data, resolve("success"));
+  })
+}
+
+function getBookmarkLevel(bookmark_id){
+  return new Promise(function(resolve, reject){
+    chrome.storage.local.get(bookmark_id, function(bookmark_level){
+      resolve(bookmark_level);
+      reject("failed");
+    })
+  })
+}
+
+function getStorage(){
+  return new Promise(function(resolve,reject){
+    chrome.storage.local.get(function(items){
+      resolve(items);
     });
   });
 }
--- popup.js
+++ popup.js
@@ -1,17 +1,44 @@
 window.onload = function(){
 	var container = document.getElementById("container");
-	chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
-	 	console.log(response);
+	chrome.runtime.sendMessage({get_bookmark: true}, function(response) {
 	 	var bookmarks = response.bookmarks;
 	  for (var i = 0; i < bookmarks.length; i++) {
 	    if (bookmarks[i].url){
 	      var link = document.createElement("a");
+	      var button = document.createElement("button");
 
 	      link.textContent = bookmarks[i].title;
 	      link.href = bookmarks[i].url;
+	      sendBookmarkLevel(bookmarks[i].id, Math.floor(Math.random() * 3));
+				getBookmarkLevel(bookmarks[i].id).then(function(result){
+					var bookmark_level = result;
+				});
 
 	      container.appendChild(link);
 	    };
 	  };
+		getStorage().then(function(result){
+			var storage = result;
+			console.log(storage);
+		});
 	});
+	function sendBookmarkLevel(bookmark_id, level){
+		chrome.runtime.sendMessage({bookmark_level: {bookmark_id: bookmark_id, level: level}}, function(response){
+			console.log(response);
+		})
+	}
+	function getBookmarkLevel(bookmark_id){
+		return new Promise(function(resolve, reject){
+			chrome.runtime.sendMessage({getBookmarkLevel: {bookmark_id: bookmark_id}}, function(response){
+				resolve(response);
+			});
+		});
+	}
+	function getStorage(){
+		return new Promise(function(resolve, reject){
+			chrome.runtime.sendMessage({getStorage: true}, function(response){
+				resolve(response);
+			});
+		});
+	}
 }
