69be8800609f6856d7640f73bb589cfb38200257
nami634 <cigarbox.nami@gmail.com>
2016-10-04 17:55:49 +0900

    current_tabとall_bookmarksをグローバル変数で保持するように変更 & コマンドでbookmark_levelを変更可能に

--- popup.js
+++ popup.js
@@ -1,39 +1,57 @@
+var current_tab,
+		all_bookmarks = [],
+		storage;
 window.onload = function(){
 	var container = document.getElementById("search-results-container"); //changed ID
 	var form = document.getElementById("search-keyword-field");
 	var new_bookmark = document.getElementById("add-new-bookmarks-button");
 
+	window.addEventListener("keydown", keyDown, false)
 	form.addEventListener("input", searchEvent, false);
 	new_bookmark.addEventListener("click", newBookmarkEvent);
+
+	getBookmarkAll().then(function (bookmarks) {
+		all_bookmarks = bookmarks;
+	});
+	getCurrentTab().then(function (current) {
+		current_tab = current;
+	});
 };
+
+function keyDown(e) {
+	if ((e.ctrlKey && !e.metaKey) || (!e.ctrlKey && e.metaKey)) {
+		if (e.key == '1' || e.key == '2' || e.key == '3' || e.key == '4') {
+			var bookmark = searchBookmarkUrl(all_bookmarks, current_tab.url);
+			console.log(bookmark);
+			if (!bookmark) return;
+			sendBookmarkLevel(bookmark.id, e.key);
+		}
+	}
+}
 
 function searchEvent(e) {
 	var container = document.getElementById("search-results-container");
 	while(container.firstChild) container.removeChild(container.firstChild);
 	var keyword = document.getElementById("search-keyword-field").value;
-	console.log('Read');
-	getBookmarkAll().then(function (result) {
-		var bookmarks = [];
-		console.log(result);
-		searchBookmark(result, keyword, bookmarks);
-		console.log(bookmarks);
-		for(var i = 0; i < bookmarks.length; i++){
-			var link = document.createElement("a");
-			var button = document.createElement("button");
-			link.setAttribute("class","searchresult");
-			link.textContent = bookmarks[i].title;
-			link.href = bookmarks[i].url;
-			container.appendChild(link);
-		}
-	});
+	var result_bookmarks = [];
+	searchBookmarkName(all_bookmarks, keyword, result_bookmarks);
+	console.log(result_bookmarks);
+	for(var i = 0; i < result_bookmarks.length; i++){
+		var link = document.createElement("a");
+		var button = document.createElement("button");
+		link.setAttribute("class","searchresult");
+		link.textContent = result_bookmarks[i].title;
+		link.href = result_bookmarks[i].url;
+		container.appendChild(link);
+	}
 	e.preventDefault();
 }
 
 function newBookmarkEvent(e) {
-	getCurrentTab().then(function (result) {
-		newBookmark(result.title, result.url, 2, 2).then(function (result) {
-			console.log(result);
-		});
+	if (current_tab == null) return;
+	console.log("read");
+	newBookmark(current_tab.title, current_tab.url, 2, 2).then(function (result) {
+		console.log(result);
 	});
 }
 
@@ -57,8 +75,10 @@ function getBookmarkAll() {
 // ブックマークのレベルをstorageに登録するためのメソッド
 function sendBookmarkLevel(bookmark_id, level){
 	chrome.runtime.sendMessage({bookmark_level: {bookmark_id: bookmark_id, level: level}}, function(response){
-		console.log(response);
-	})
+		getBookmarkLevel(bookmark_id).then(function (bookmark_level) {
+			console.log(bookmark_level);
+		});
+	});
 }
 // ブックマークのレベルをstorageから取ってくるメソッド
 function getBookmarkLevel(bookmark_id){
@@ -72,9 +92,14 @@ function getBookmarkLevel(bookmark_id){
 function newBookmark(title, url, parent, level) {
 	return new Promise(function (resolve, reject) {
 		chrome.runtime.sendMessage({newBookmark: {title: title, url: url, parent: parent, level: level}}, function (bookmark) {
+			console.log(bookmark);
 			if (bookmark.id){
 				getBookmarkLevel(bookmark.id).then(function (bookmark_level) {
-					resolve([bookmark, bookmark_level]);
+					getBookmarkAll().then(function (bookmarks) {
+						console.log(bookmarks);
+						all_bookmarks = bookmarks;
+						resolve([bookmark, bookmark_level, all_bookmarks]);
+					});
 				});
 			}
 		});
@@ -91,13 +116,30 @@ function getStorage(){
 }
 
 // bookmark検索メソッド
-function searchBookmark(bookmarks, name, result){
+function searchBookmarkName(bookmarks, name){
 	result = result || [];
 	bookmarks.forEach(function (val, index, array) {
 		if(val.children){
-			searchBookmark(val.children, name, result);
+			searchBookmarkName(val.children, name, result);
 		} else if(val.url && (new RegExp(name, 'i')).test(val.title) && result.length < 10) {
 			result.push(val);
 		}
 	});
+}
+
+function searchBookmarkUrl(bookmarks, url) {
+	for (let i = 0; i < bookmarks.length; i++){
+		let val = bookmarks[i];
+		let result;
+		if (val.children){
+			result = searchBookmarkUrl(val.children, url);
+		} else if(val.url == url){
+			console.log(val);
+			result = val;
+		}
+
+		if(result) return result;
+	}
+
+	return false;
 }
