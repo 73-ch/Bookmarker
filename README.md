86331b125e5f24acc03c288d694015fa59a9db86
Meguna <meguna@Megunas-MacBook-Pro.local>
2016-11-01 19:09:00 +0900

    edit

--- README.md
+++ README.md
@@ -13,3 +13,41 @@
 - windowごとupdate
 	- project単位
 - 管理画面
+
+
+
+### todo
+- icon
+
+- popup
+    - 選択できるようにする
+    - 1~4のボタンで作成とレベルの表示・変更(label)
+    - favicon title url folder project
+    - 検索を小さく
+    - projectのwindow丸ごと作成のボタン
+
+-　bookmark管理画面
+    - title・url・level・projectの変更
+
+- user_settings
+    - result_max
+    - folders
+    - result種類
+    - commands
+        - command + i
+            ブックマークの削除
+
+        popupの中
+        - command + 1~4
+            各レベルのブックマークの作成
+            レベルの変更
+            * 4はproject用だからどのprojectに登録するか選べるようにする必要あり
+        - command + p
+            search_barのnameのprojectを作成
+        - 上下キーとtab
+            検索結果を選ぶ
+        - enter
+            選ばれている検索結果を現在のタブで開く
+        - shift + enter
+            選ばれている検索結果を新しいのタブで開く
+
--- popup.js
+++ popup.js
@@ -2,12 +2,12 @@ var current_tab,
 		all_bookmarks = [],
 		selected_content = -1,
 		result_max = 20,
-		storage,
 		result_bookmarks = [],
 		all_windows = [],
-		all_tabs = [];
+		all_tabs = [],
+		all_projects = [];
 window.onload = function(){
-	var container = document.getElementById("search-results-container"); //changed ID
+	var container = document.getElementById("search-results-container");
 	var form = document.getElementById("search-keyword-field");
 	var new_bookmark = document.getElementById("add-new-bookmarks-button");
 
@@ -16,7 +16,6 @@ window.onload = function(){
 	new_bookmark.addEventListener("click", newBookmarkEvent);
 
 	getBookmarkAll().then(function (bookmarks) {
-		console.log(bookmarks);
 		all_bookmarks = bookmarks;
 	});
 	getCurrentTab().then(function (current) {
@@ -24,9 +23,14 @@ window.onload = function(){
 	});
 	getAllWindow().then(function (windows) {
 		all_windows = windows;
-		console.log(all_windows);
 		getAllTabs(all_windows, all_tabs);
-		console.log(all_tabs);
+	});
+	getAllProject().then(function (projects) {
+		if (projects){
+			all_projects = projects;
+		}else{
+			all_projects = [];
+		}
 	});
 };
 
@@ -43,17 +47,23 @@ function keyDown(e) {
 				});
 			}
 			window.close();
+		}else if (e.keyCode == 80){
+			let name = document.getElementById("search-keyword-field").value;
+			newProject(name, current_tab.windowId);
+			e.preventDefault();
 		}
 	}
 	if (e.keyCode == 13) {
 		var result = result_bookmarks[selected_content];
 		if(result){
 			if (result.tab){
-				console.log('read');
 				openTab(result);
 			}else if(result.bookmark) {
-				createTab(result.url);
+				createTab(result.url, e.shiftKey);
+			}else if(result.project){
+				openProject(result.id);
 			}
+			e.preventDefault();
 		}
 	}else if (e.keyCode == 9 || e.keyCode == 40){
 		console.log(selected_content);
@@ -72,21 +82,47 @@ function keyDown(e) {
 }
 
 function searchEvent(e) {
+	selected_content = 0;
 	var container = document.getElementById("search-results-container");
 	while(container.firstChild) container.removeChild(container.firstChild);
 	var keyword = document.getElementById("search-keyword-field").value;
 	result_bookmarks = [];
-	searchTabName(all_tabs, keyword, result_bookmarks);
+	if (all_tabs.length > 0)searchTabName(all_tabs, keyword, result_bookmarks);
+	if (result_bookmarks.length <= result_max && all_projects.length > 0) searchProjectName(all_projects, keyword, result_bookmarks);
 	if (result_bookmarks.length <= result_max) searchBookmarkName(all_bookmarks, keyword, result_bookmarks);
 	if (result_bookmarks.length <= result_max) searchBookmarkUrl(all_bookmarks, keyword, result_bookmarks);
 	for(var i = 0; i < result_bookmarks.length; i++){
+	  // bookmarkとかを格納するdiv
+	  var result = document.createElement("div");
+    result.setAttribute("class", "result");
+    if (result_bookmarks[i].tab){
+      result.setAttribute("class", "tab");
+    }else if (result_bookmarks[i].bookmark){
+      result.setAttribute("class", "bookmark");
+    }
+    // bookmarkのtitle
 		var link = document.createElement("a");
-		var button = document.createElement("button");
 		link.setAttribute("class","searchresult");
-		if(!result_bookmarks[i].title) result_bookmarks[i].title = "(no name)";
+		if(!result_bookmarks[i].title) result_bookmarks[i].title = "(no name)";// titleがない場合の代わり
 		link.textContent = result_bookmarks[i].title;
 		link.href = result_bookmarks[i].url;
-		container.appendChild(link);
+    result.appendChild(link);
+    // bookmarkのURL(projectにはない)
+    if(result_bookmarks[i].tab || result_bookmarks[i].bookmark){
+      var url = document.createElement("p");
+      url.textContent = result_bookmarks[i].url;
+      result.appendChild(url);
+    }
+    // bookmarkのfavicon(projectにはない)
+    var favicon = document.createElement("img");
+    if (result_bookmarks[i].tab){
+      favicon.src = result_bookmarks[i].favIconUrl;
+    }else if (result_bookmarks[i].bookmark){
+      favicon.src = "chrome://favicon/" + result_bookmarks[i].url;
+    }
+    result.appendChild(favicon);
+
+    container.appendChild(result);
 	}
 	e.preventDefault();
 }
@@ -98,10 +134,17 @@ function newBookmarkEvent(e) {
 	});
 }
 
-function createTab(url) {
-	chrome.tabs.create({url: url}, function (tab) {
-		console.log(tab);
-	});
+function createTab(url, new_tab) {
+	if(new_tab){
+		chrome.tabs.create({url: url}, function (tab) {
+			console.log(tab);
+		});
+	}else{
+		chrome.tabs.update(current_tab.id, {url: url}, function (tab) {
+			console.log(tab);
+			window.close();
+		});
+	}
 }
 
 function openTab(tab) {
@@ -236,4 +279,36 @@ function getAllWindow() {
 			resolve(response);
 		});
 	});
+}
+
+function getAllProject() {
+	return new Promise(function (resolve) {
+		chrome.runtime.sendMessage({getAllProject: true}, function (response) {
+			resolve(response);
+		});
+	});
+}
+
+function newProject(name, windowId) {
+	return new Promise(function (resolve) {
+		chrome.runtime.sendMessage({newProject: {name: name, windowId: windowId}}, function (response) {
+			resolve(response);
+		});
+	});
+}
+
+function searchProjectName(projects, name, result) {
+	result = result || [];
+	for(let i = 0; i < projects.length; i++){
+		let val = projects[i];
+		if ((new RegExp(name, 'i')).test(val.title)){
+			val.project = true;
+			if (result.indexOf(val) < 0)result.push(val);
+		}
+		if (result.length >= result_max)break;
+	}
+}
+
+function openProject(project_id) {
+	chrome.runtime.sendMessage({openProject: {project_id: project_id}});
 }
